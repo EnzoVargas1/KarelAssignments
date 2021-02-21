1140ce7b904bbdd77d3f9fd7ee5930423eb8be50
Enzo Vargas <49962569+EnzoVargas1@users.noreply.github.com>
2021-02-21 13:32:10 -0600

    Update DrawingLines.java

--- DrawingLines.java
+++ DrawingLines.java
@@ -1,7 +1,40 @@
 package week2;
 
 import acm.program.GraphicsProgram;
+import java.awt.event.MouseEvent;
+
+import javax.sound.sampled.Line;
+
+import acm.graphics.GLine;
+import acm.program.*;
+
 
 public class DrawingLines extends GraphicsProgram {
+	
+	public void run() {
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		 firstX = e.getX();
+		 firstY = e.getY();
+		 line = new GLine(firstX, firstY, firstX, firstY);
+	     add(line);
 
+		}
+	
+	    public void mouseDragged(MouseEvent e) {
+	    	lastX = e.getX();
+	    	lastY = e.getY();
+	        line.setEndPoint(lastX, lastY);
+	    }
+	    
+	
+	
+    private GLine line;
+	private double firstX;
+	private double firstY;
+	private double lastX;
+	private double lastY;
+	
 }
