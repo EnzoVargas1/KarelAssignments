c9894384096833db2b3d1713376559bdf49ad096
Enzo Vargas <49962569+EnzoVargas1@users.noreply.github.com>
2021-02-21 13:31:29 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,8 +4,65 @@ import acm.program.*;
 
 public class Fibonacci extends ConsoleProgram {
 
+	private static int MAX_TERM_VALUE = 10000;
 	
 	public void run() {
-
+		println("Printing the Fibonacci Sequence recursively:");
+		fib(0);
+		println();
+		println("Printing the Fibonacci Sequence iteratively:");
+		Fib(0);
 	}
+	
+	//iterative
+	public void Fib(int num) {
+		int prev = num;
+		int temp = num;
+		
+		println(num);
+		num+=1;
+		prev = temp;
+		temp = num;
+		
+		while(num < 10000) {
+			println(num);
+			num = prev + temp; 
+			prev = temp;
+			temp  = num;
+			
+		}
+				
+	}
+	
+	//recrusive
+	public void fib(int num) {
+		int prev = num;
+		int temp = num;
+		
+	    println(num);
+		num+=1;
+		prev = temp;
+		temp = num;
+
+		recurFib(num, prev, temp);
+	}
+	
+	
+	public void recurFib(int num, int prev, int temp) {
+		 
+		if(num < MAX_TERM_VALUE) {
+			println(num);
+			num = prev + temp; 
+			prev = temp;
+			temp  = num;
+			recurFib(num, prev, temp);
+
+		}
+		else {
+			return;
+		}
+		
+	}
+	
+	
 }
