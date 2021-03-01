72cba1318f495626421c66928fa1dc1a4bc71dd4
Enzo Vargas <49962569+EnzoVargas1@users.noreply.github.com>
2021-03-01 15:21:13 -0600

    Update DeleteCharactersFromString.java

--- DeleteCharactersFromString.java
+++ DeleteCharactersFromString.java
@@ -5,7 +5,22 @@ import acm.program.ConsoleProgram;
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		println(removeAllOccurrences("This is a test", 't'));
+		println(removeAllOccurrences("Summer is here!", 'e'));
+		println(removeAllOccurrences("---0---", '-'));
+	}
+	
+	public String removeAllOccurrences(String str, char ch) {
+		String newStr = "";
+		
+		for(int i = 0; i < str.length(); i++) {
+			if(!(ch == str.charAt(i))) {
+				newStr = newStr + str.charAt(i) ;
+			}
+		}
+		
+		return newStr;
 	}
 	
 }
+
