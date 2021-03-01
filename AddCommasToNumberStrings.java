973079409df75888295a2020573bfeb6fd2d66b2
Enzo Vargas <49962569+EnzoVargas1@users.noreply.github.com>
2021-03-01 15:20:03 -0600

    Update AddCommasToNumberStrings.java

--- AddCommasToNumberStrings.java
+++ AddCommasToNumberStrings.java
@@ -5,7 +5,30 @@ import acm.program.ConsoleProgram;
 public class AddCommasToNumberStrings extends ConsoleProgram {
 	
 	public void run() {
-
+		while(true) {
+			String input = readLine("Enter a number: ");
+			if (input.length() == 0) break;
+			println(addCommasToNumericDigits(input));
+		}
+	}
+	
+	private String addCommasToNumericDigits(String str) {
+		String newStr = "";
+		int count = 0;
+		for(int i = str.length()-1; i>=0; i--) {
+			char ch = str.charAt(i);
+			count++;
+			
+			if(count%3 == 0 && i != 0) {
+				newStr = ch + newStr;
+				newStr = "," + newStr;
+			}
+			else {
+				newStr = ch + newStr;
+			}
+		}
+		
+		return newStr;
 	}
 	
 }
